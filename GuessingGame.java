f460b35d0f1072f5f07fdf84100784daf1df297d
Charmeister11 <tsutavicius@gmail.com>
2021-09-18 17:34:35 +0200

    comments added

--- GuessingGame.java
+++ GuessingGame.java
@@ -4,7 +4,7 @@
  * @id <<1704915>>
  * @author <<Gabrielius Rosinas>>
  * @id <<1655175>>
- * @date 16/09/2021
+ * @date <<16/09/2021>>
  */
 
 import java.util.*; // for Scanner and Random
@@ -15,15 +15,16 @@ class GuessingGame {
 
     void run() {
         // TODO: Implementation 
-        String choice;
-        int number = 0; // "code"
-        long seed;
-        int guess;
-        int counter = 0;
-        boolean correct = false; 
+        String choice; // PLAYER'S CHOICE (YES // NO)
+        int number = 0; // SECRET NUMBER TO BE GUESSED
+        long seed; // SEED FOR RANDOM NUM GENERATION
+        int guess; // GUESS OF THE PLAYER
+        int counter = 0; // AMOUNT OF GUESSES
+        boolean correct = false; // KEEPS TRACK IF PLAYER GUESSED CORRECTLY
+        boolean hasItDied = false;// DETERMINES IF THE CODE SHOULD KEEP RUNNING
         String[][] historyTable;
-        historyTable = new String[7][100]; 
-        boolean hasItDied = false;//TRUE IF WRONG INPUT INITIALLY
+        historyTable = new String[7][100]; // 2D ARRAY OF GUESSING HISTORY
+       
 
         System.out.println("Do you want to enter the secret code yourself ?"); // QUESTIONNAIRE TIME BABY
         choice = scanner.next();
@@ -38,11 +39,10 @@ class GuessingGame {
             number = randomGenerator.nextInt(100);
             System.out.println("Start Guessing!");
         } else {
-            System.out.println("fuck off");
             hasItDied = true;
         }
         
-        for (int i=0; i<7; i++){ // INITIALIZES TABLE WITH DOTS
+        for (int i=0; i<7; i++){ // INITIALIZES 2D ARRAY WITH DOTS
             for(int j=0; j<100; j++){
                 historyTable[i][j] = (".");
             }
@@ -50,7 +50,7 @@ class GuessingGame {
 
         while (!(correct) && (counter < 7) && !(hasItDied)) { // THE GAME ITSELF
             guess = scanner.nextInt();
-            if(guess > number) {
+            if(guess > number) { // CONDITIONALS FOR GUESSES THAT ARE HIGHER/LOWER/CORRECT
                 System.out.println("lower");
             } else if(guess < number){
                 System.out.println("higher");
@@ -58,9 +58,9 @@ class GuessingGame {
                 System.out.println("Good guess! You won.");
                 correct = true;
             }
-            historyTable[counter][number] = "|";
-            historyTable[counter][guess] = "X";
-            counter = counter + 1;
+            historyTable[counter][number] = "|"; // CORRECT NUMBER POSITION IN 2D ARRAY
+            historyTable[counter][guess] = "X"; // GUESS POSITION IN 2D ARRAY
+            counter = counter + 1; // GUESS COUNT
             if (counter == 7) {
                 System.out.println("No more guesses, you lost.");
             }
--- GuessingGameAdvanced.java
+++ GuessingGameAdvanced.java
@@ -1,11 +1,11 @@
 
 /**
  * GuessingGameAdvanced -- second part of assignment 2 in 2ip90
- * @author TODO
- * @id     TODO
- * @author TODO
- * @id     TODO
- * @date   TODO
+ * @author <<Tomas Sutavicius>>
+ * @id     <<1704915>>
+ * @author <<Gabrielius Rosinas>>
+ * @id     <<1655175>>
+ * @date   <<16/09/2021>>
  */
 
 import java.util.Scanner;
@@ -16,33 +16,33 @@ class GuessingGameAdvanced {
 
     void run() {
         // TODO: Implementation
-        String adjustBoundary;
-        int counter = 0;
-        boolean correct = false;
+        String adjustBoundary; // PLAYER INPURS HIGHER/H OR LOWER/L
+        int counter = 0; // GUESS COUNTER 
+        boolean correct = false; // KEEPS TRACK IF PLAYER GUESSED CORRECTLY
         Random randomGenerator = new Random();
-        int number = randomGenerator.nextInt(1001);
+        int number = randomGenerator.nextInt(1000); // PROGRAMS INITIAL GUESS
         int limit[];
-        limit = new int[2]; // lower and upper bounds of possible guesses for computer
-        limit[1] = 1001;
-        limit[0] = 0;
+        limit = new int[2]; // LOWER AND UPER BOUNDS OF POSSIBLE GUESSES FOR COMPUTER
+        limit[1] = 1000; // INITIAL UPPER BOUND
+        limit[0] = 0; // INITIAL LOWER BOUND
 
         System.out.println(
-                "Think of a secret number not smaller than 0 and not greater than 1000. Type ’go’ when you have one.");
+            "Think of a secret number not smaller than 0 and not greater than 1000. Type ’go’ when you have one.");
         if (scanner.next().equals("go")) {
             while (!(correct) && (counter < 10)) { // THE GAME ITSELF
                 System.out.println(number);
                 adjustBoundary = scanner.next();
-                if (adjustBoundary.contains("l")) {// if lower than your number
-                    limit[1] = number - 1;
-                    number = randomGenerator.nextInt(limit[1] - limit[0] + 1) + limit[0];// If random generates 0, lower
-                                                                                         // bound will be added
-                } else if (adjustBoundary.contains("h")) {//if higher than your nuber
-                    limit[0] = number + 1;
+                if (adjustBoundary.contains("l")) {// IF YOUR NUMBER IS LOWER THAN PROGRAMS GUESS
+                    limit[1] = number - 1; // SETS NEW UPPER BOUND
+                    number = randomGenerator.nextInt(limit[1] - limit[0] + 1) + limit[0]; // NEW RANDOM GUESS BASED ON
+                                                                                          // NEW BOUNDS
+                } else if (adjustBoundary.contains("h")) {// IF YOUR NUMBER IS HIGHER THAN PROGRAMS GUESS
+                    limit[0] = number + 1; // SETS NEW LOWER BOUND
                     number = randomGenerator.nextInt(limit[1] - limit[0] + 1) + limit[0];
-                } else if (adjustBoundary.equals("cum")) {
+                } else if (adjustBoundary.equals("Good guess! You won.")) {
                     correct = true;
                 }
-                counter = counter + 1;
+                counter = counter + 1; // GUESS COUNT
                 if (counter == 10) {
                     System.out.println(number);
                     System.out.println("I give up. This should not happen!");
