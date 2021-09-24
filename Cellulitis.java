e1da10eaf638d156a87e7aa3c63c9c7d6b77fb8e
Charmeister11 <tsutavicius@gmail.com>
2021-09-24 16:40:31 +0200

    Update Cellulitis.java

--- Cellulitis.java
+++ Cellulitis.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Cellulitis2 {
+public class Cellulitis {
     Scanner sc = new Scanner(System.in);
     String type = sc.next(); // type of automaton
     int length = sc.nextInt(); // length of row, doesnt count extra border cells
     int gens = sc.nextInt(); // number of generations
-    boolean[] currentGen = new boolean[length+2];
-    int[] rules = new int[8];
+    boolean[] currentGen = new boolean[length+2]; // init boolean array of length + 2 to account for borders
+    //int[] rules = new int[8]; TODO remove
     boolean[] rulesBool = new boolean[8];
 
 
-    void draw(boolean[] generation){ //draws current generation
+    void draw(boolean[] generation){ //prints current generation
         for (int i = 0; i < length + 2; i++ ){
             if (generation[i]){
                 System.out.print("*");
@@ -21,24 +21,20 @@ public class Cellulitis2 {
         System.out.println();
     }
 
-    boolean patternA(String z){ // pattern
+    boolean patternA(String z){ // pattern A
         switch(z){
-            case ("001"):
-                return true;
-            case ("011"):
-                return true;
-            case ("100"):
-                return true;
-            case ("101"):
-                return true;
-            case ("110"):
-                return true;
-            default:
+            case ("111"):
                 return false;
+            case ("010"):
+                return false;
+            case ("000"):
+                return false;
+            default:
+                return true;
         }
     }
 
-    boolean patternB(String z){
+    boolean patternB(String z){ // pattern B
         switch(z){
             case ("001"):
                 return true;
@@ -53,7 +49,7 @@ public class Cellulitis2 {
         }
     }
 
-    boolean patternU(String z) {
+    boolean patternU(String z) { // custom pattern U
         switch(z){
             case ("000"):
                 return rulesBool[0];
@@ -79,12 +75,12 @@ public class Cellulitis2 {
     }
 
     boolean[] calcNextGen(boolean[] curGen){ // calculates the next generation and returns it 
-        boolean[] nextGen = new boolean[length+2];
+        boolean[] nextGen = new boolean[length+2]; // initializes/resets nextGen array
         for (int i = 1; i<length+1; i++){
             int left = (curGen[i-1]) ? 1 : 0;
             int mid = (curGen[i]) ? 1 : 0;
             int right = (curGen[i+1]) ? 1 : 0;
-            String pattern = String.valueOf(left) + String.valueOf(mid) + String.valueOf(right);
+            String pattern = String.valueOf(left) + String.valueOf(mid) + String.valueOf(right); //creates cell neighbourhood pattern
             if (type.equals("A")){
                 nextGen[i] = patternA(pattern);
             } else if (type.equals("B")) {
@@ -96,7 +92,7 @@ public class Cellulitis2 {
         return nextGen;
     }
 
-    void init(){
+    void init(){ //initializes all inputs 
         String start = sc.next();
         if (start.equals("init_start")) {
             String input = sc.next();
@@ -108,10 +104,10 @@ public class Cellulitis2 {
                 input = sc.next();
             }
         }
-        if (type.equals("U")) {
+        if (type.equals("U")) { // initializes the rules for automaton type U
             for( int j = 0; j < 8; j++ ){
-                rules[j] = sc.nextInt();
-                if (rules[j] == 1){
+                //rules[j] = sc.nextInt();
+                if (sc.nextInt() == 1){
                     rulesBool[j] = true;
                 } else {
                     rulesBool[j] = false;
@@ -123,16 +119,13 @@ public class Cellulitis2 {
     void main(){
         init();
         for (int i = 0; i < gens; i++){
-            draw(currentGen);
-            currentGen = calcNextGen(currentGen);          
+            draw(currentGen); // draws current generation
+            currentGen = calcNextGen(currentGen); // equates current generation to the next generation  
         }
         
     }
 
     public static void main(String[] args) {
-        (new Cellulitis2()).main();
+        (new Cellulitis()).main();
     }
-
-
-
 }
