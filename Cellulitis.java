676fa39676ff13a144a5a8bca83d4c4ecf789658
Charmeister11 <tsutavicius@gmail.com>
2021-09-24 16:09:03 +0200

    bruv

--- Cellulitis.java
+++ Cellulitis.java
@@ -1,75 +1,17 @@
-/**
- * Homework Assignment 3 2ip90
- *  
- * @author <<Tomas Sutavicius>>
- * @id <<1704915>>
- * @author <<Gabrielius Rosinas>>
- * @id <<1655175>>
- * @group <<68>>
- * @date <<23/09/2021>>
- */
-
 import java.util.Scanner;
-import java.util.Arrays;
 
-public class Cellulitis {
+public class Cellulitis2 {
     Scanner sc = new Scanner(System.in);
-    String x = sc.next(); // type of automaton
-    int L = sc.nextInt(); // length of row, doesnt count extra border cells
-    int G = sc.nextInt(); // number of generations
-    boolean[] tempGeneration = new boolean[L+2];
-    boolean[] currentGeneration = new boolean[L+2];
+    String type = sc.next(); // type of automaton
+    int length = sc.nextInt(); // length of row, doesnt count extra border cells
+    int gens = sc.nextInt(); // number of generations
+    boolean[] currentGen = new boolean[length+2];
+    int[] rules = new int[8];
+    boolean[] rulesBool = new boolean[8];
 
 
-    boolean runA(String z){
-        switch(z){
-            case ("000"):
-                return false;
-            case ("001"):
-                return true;
-            case ("010"):
-                return false;
-            case ("011"):
-                return true;
-            case ("100"):
-                return true;
-            case ("101"):
-                return true;
-            case ("110"):
-                return true;
-            case ("111"):
-                return false;
-            default:
-                return false;
-        }
-    // for (int i = 1; )
-    //     if(currentGeneration)
-
-        // if (cur) {
-        //     if (next && prev) {
-        //         cur = // 7
-        //     } else if (next) {
-
-        //     } else if (prev)
-        // } else {
-        //     if (next && prev) {
-        //         cur = // 7
-        //     } else if (next) {
-
-        //     } else if (prev)
-        // }
-    }
-
-    void runB(){ //stfu
-
-    }
-
-    void runU(){ //stfu
-
-    }
-
-    void draw(boolean[] generation) { // prints current generation
-        for (int i = 1; i < L + 2; i++ ){
+    void draw(boolean[] generation){ //draws current generation
+        for (int i = 0; i < length + 2; i++ ){
             if (generation[i]){
                 System.out.print("*");
             } else {
@@ -78,64 +20,119 @@ public class Cellulitis {
         }
         System.out.println();
     }
-    
-    boolean[] nextGenerationA(boolean[] generation){ //returns next generation (hypothetically) // inputs an array, returns an array // something is fucky wucky with this function
-        Arrays.fill(tempGeneration, false);
-        // Everyting seems to check out but something is really wrong 
-        // How does this even happen >_>
-        // trying to figure this out gives me cancer .-.
 
-        for (int i = 1; i < L + 1; i++) {
-            // builds the cage of 3 
-            int left = (generation[i-1]) ? 1 : 0;
-            int mid = (generation[i]) ? 1 : 0;
-            int right = (generation[i+1]) ? 1 : 0;
-
-            // passes the values of the cage to the swtich case function
-            String pattern = String.valueOf(left) + String.valueOf(mid) + String.valueOf(right);
-            //System.out.print(pattern+ " ");
-
-            // changes the bools based on the results of the switch cases
-            runA(pattern);
-            generation[i] = runA(pattern);
-        
+    boolean patternA(String z){ // pattern
+        switch(z){
+            case ("001"):
+                return true;
+            case ("011"):
+                return true;
+            case ("100"):
+                return true;
+            case ("101"):
+                return true;
+            case ("110"):
+                return true;
+            default:
+                return false;
         }
-        //print current gen
-        //calc next gen
-        //assign next gen to current gen variable
-        //repeat
-        //...
-        //profit
-        return generation;
     }
 
-    void main(){
+    boolean patternB(String z){
+        switch(z){
+            case ("001"):
+                return true;
+            case ("010"):
+                return true;
+            case ("100"):
+                return true;
+            case ("110"):
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    boolean patternU(String z) {
+        switch(z){
+            case ("000"):
+                return rulesBool[0];
+            case ("001"):
+                return rulesBool[1];
+            case ("010"):
+                return rulesBool[2];
+            case ("011"):
+                return rulesBool[3];
+            case ("100"):
+                return rulesBool[4];
+            case ("101"):
+                return rulesBool[5];
+            case ("110"):
+                return rulesBool[6];
+            case ("111"):
+                return rulesBool[7];
+            default:
+                return false;
+        }
+
+
+    }
+
+    boolean[] calcNextGen(boolean[] curGen){ // calculates the next generation and returns it 
+        boolean[] nextGen = new boolean[length+2];
+        for (int i = 1; i<length+1; i++){
+            int left = (curGen[i-1]) ? 1 : 0;
+            int mid = (curGen[i]) ? 1 : 0;
+            int right = (curGen[i+1]) ? 1 : 0;
+            String pattern = String.valueOf(left) + String.valueOf(mid) + String.valueOf(right);
+            if (type.equals("A")){
+                nextGen[i] = patternA(pattern);
+            } else if (type.equals("B")) {
+                nextGen[i] = patternB(pattern);
+            } else if (type.equals("U")){
+                nextGen[i] = patternU(pattern);
+            }   
+        }
+        return nextGen;
+    }
+
+    void init(){
         String start = sc.next();
         if (start.equals("init_start")) {
             String input = sc.next();
             while (!(input.equals("init_end"))) {
                 int n = Integer.parseInt(input);
-                if( n < L) {
-                    currentGeneration[n+1] = true;
-                } 
+                if( n < length+1) {
+                    currentGen[n] = true;
+                }
                 input = sc.next();
             }
-        } 
-        for (int i = 0; i<G; i++){
-            // System.out.print(currentGeneration);
-            draw(currentGeneration);
-            // System.out.print(currentGeneration);
-            currentGeneration = nextGenerationA(currentGeneration); // this is the tempGeneration value returned by the nextGenerationA function
-                
-            
-            
+        }
+        if (type.equals("U")) {
+            for( int j = 0; j < 8; j++ ){
+                rules[j] = sc.nextInt();
+                if (rules[j] == 1){
+                    rulesBool[j] = true;
+                } else {
+                    rulesBool[j] = false;
+                }
+            }
+        }
+    }
+
+    void main(){
+        init();
+        for (int i = 0; i < gens; i++){
+            draw(currentGen);
+            currentGen = calcNextGen(currentGen);          
         }
         
-
     }
 
-        
     public static void main(String[] args) {
-        (new Cellulitis()).main();
+        (new Cellulitis2()).main();
     }
+
+
+
 }
