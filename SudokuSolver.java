f5a0c12a5b306ed0b39c2c18ec7b71cf4babb7cf
Charmeister11 <tsutavicius@gmail.com>
2021-10-03 23:48:33 +0200

    Co-authored-by: Rosinas, Gabrielius <g.rosinas@student.tue.nl>

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -15,17 +15,17 @@ class SudokuSolver {
     int SUDOKU_BOX_DIMENSION = 3; // Dimension of the boxes (sub-grids that should contain all digits).
 
     int[][] grid = new int[][] {  // The puzzle grid; 0 represents empty.
-        { 7, 0, 0,   4, 0, 0,    2, 0, 0 },    // One solution.
-        { 9, 3, 0,   0, 0, 0,    0, 0, 0 },
-        { 0, 0, 2,   0, 8, 0,    0, 0, 0 },
+        { 0, 0, 0,   2, 0, 0,    0, 1, 0 },    // One solution.
+        { 0, 0, 0,   0, 0, 0,    0, 7, 9 },
+        { 0, 0, 0,   1, 0, 7,    0, 3, 5 },
 
-        { 4, 0, 0,   0, 0, 2,    0, 9, 0 },
-        { 0, 0, 0,   0, 0, 0,    0, 0, 6 },
-        { 0, 0, 1,   8, 0, 6,    0, 7, 0 },
+        { 0, 1, 4,   3, 6, 0,    0, 1, 0 },
+        { 7, 0, 0,   0, 0, 0,    0, 7, 0 },
+        { 0, 0, 0,   0, 1, 2,    0, 9, 1 },
 
-        { 0, 0, 0,   0, 0, 3,    0, 0, 0 },
-        { 0, 0, 0,   9, 0, 5,    0, 0, 0 },
-        { 0, 0, 0,   0, 0, 0,    0, 4, 0 },
+        { 0, 9, 0,   4, 0, 0,    0, 0, 0 },
+        { 0, 0, 6,   7, 0, 0,    0, 0, 0 },
+        { 0, 0, 0,   0, 0, 6,    0, 0, 0 },
     };
     
     int[][] finalGrid = new int[9][9];
@@ -41,8 +41,8 @@ class SudokuSolver {
 
     // Is there a conflict when we fill in d in row r?
     boolean rowConflict(int r, int d) { // Returns true if there is a conflict
-        for (int i = 0; i < 9; i++){
-            if (grid[r][i] == d){
+        for (int i = 0; i < 9; i++) {
+            if (grid[r][i] == d) {
                 return true;
             } 
         }
@@ -52,7 +52,7 @@ class SudokuSolver {
     // Is there a conflict in column c when we fill in d?
     boolean columnConflict(int c, int d) {
         for (int i = 0; i < 9; i++) {
-            if(grid[i][c] == d) {
+            if (grid[i][c] == d) {
                 return true;
             }
         }
@@ -61,14 +61,14 @@ class SudokuSolver {
 
     // Is there a conflict in the box at (r, c) when we fill in d?
     boolean boxConflict(int r, int c, int d) {
-        int rowStart = (r/SUDOKU_BOX_DIMENSION) * SUDOKU_BOX_DIMENSION; // if boxrow 1 -> starts at 0, 2 -> s/a 3, 3 s/a -> 6
-        int rowEnd = rowStart + SUDOKU_BOX_DIMENSION;
-        int colStart = (c/SUDOKU_BOX_DIMENSION) * SUDOKU_BOX_DIMENSION; 
+        int rowStart = (r/SUDOKU_BOX_DIMENSION) * SUDOKU_BOX_DIMENSION; // rowStart of r[0:2] = 0, rowStart of r[3:5] = 3, rowStart of r[6:8] = 6 
+        int rowEnd = rowStart + SUDOKU_BOX_DIMENSION; // rowEnd of r[0:2] = 3, rowStart of r[3:5] = 6, rowStart of r[0:2] = 9 
+        int colStart = (c/SUDOKU_BOX_DIMENSION) * SUDOKU_BOX_DIMENSION; // same thinking as above for the rest
         int colEnd =  colStart + SUDOKU_BOX_DIMENSION;
         
         for (int i = rowStart; i < rowEnd ; i++) {
             for (int j = colStart; j < colEnd; j++) {
-                if(grid[i][j] == d) {
+                if (grid[i][j] == d) {
                     return true;
                 }
             }
@@ -77,35 +77,34 @@ class SudokuSolver {
     }
 	
 	// Is there a conflict in the asterisk when we fill in d?
-	boolean asteriskConflict(int r, int c, int d){
+	boolean asteriskConflict(int r, int c, int d) {
         int[][] asteriksLocation = new int[][] {{1,4},{2,2},{2,6},{4,1},{4,4},{4,7},{6,2},{6,6},{7,4}};// Array containing location of all squares in asterisk
         boolean location = false;
         for (int i = 0; i < asteriksLocation.length; i++ ){
-                if( r == asteriksLocation[i][0] && c == asteriksLocation[i][1]) {
+                if( r == asteriksLocation[i][0] && c == asteriksLocation[i][1]) { // Checks if r and c match any of the asterisk locations 
                     location = true;
                     break;
                 }
         }
         if (location) {
-            for(int i = 0; i < asteriksLocation.length; i++ ) {
+            for (int i = 0; i < asteriksLocation.length; i++ ) { // Goes through every element checking if values in the asterisk locations match d
                 int a = asteriksLocation[i][0];
                 int b = asteriksLocation[i][1];
-                if(grid[a][b] == d) {
+                if (grid[a][b] == d) {
                     return true;
                     
                 }
             }
         }
-        //System.out.println(bool);
         return false;
 
     }
 
 	// Finds the next empty square (in "reading order").
     int[] findEmptySquare() {
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 9; j++){
-                if (grid[i][j] == 0){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (grid[i][j] == 0) {
                     return new int[]{i,j};
                 }
             }
@@ -119,10 +118,10 @@ class SudokuSolver {
         int r = emptySquare[0];
         int c = emptySquare[1];
         for (int i = 1; i <= 9; i++) {
-            if(r != -1) {
-                if (!givesConflict(r, c, i)) {
+            if (r != -1) { // checks if there is an available next cell
+                if (!givesConflict(r, c, i)) { // checks whether or not there is a conflict 
                     grid[r][c] = i;
-                    if (findEmptySquare()[0] == -1 ) {
+                    if (findEmptySquare()[0] == -1 ) { // Checks if next cell is final cell which means the sudoku is solved
                         firstSolution();
                         solutionCounter++;
                 }
@@ -133,7 +132,7 @@ class SudokuSolver {
         } 
     }
 
-    void firstSolution() {
+    void firstSolution() { // Copies current (finished) grid into finalGrid
         finalGrid = new int[grid.length][grid.length];
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -143,20 +142,19 @@ class SudokuSolver {
     }
 
     // Print the sudoku grid.
-    void print(){
+    void print() {
         grid = finalGrid;
-        System.out.println("+-----------------+");
-        
-        for (int i = 0; i < 9; i++){
-            if (i == 1 || i == 7){
+        System.out.println("+-----------------+"); // Top line
+        for (int i = 0; i < 9; i++) {
+            if (i == 1 || i == 7) {
                 String test = String.format("|%d %d %d|%d>%d<%d|%d %d %d|", 
                 grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8]);
                 System.out.println(test);
-            } else if (i == 2 || i == 6){
+            } else if (i == 2 || i == 6) {
                 String test = String.format("|%d %d>%d|%d %d %d|%d<%d %d|",
                 grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8]);
                 System.out.println(test);
-            } else if (i==4){
+            } else if (i==4) {
                 String test = String.format("|%d>%d<%d|%d>%d<%d|%d>%d<%d|", 
                 grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8]);
                 System.out.println(test);
@@ -165,8 +163,8 @@ class SudokuSolver {
                 grid[i][0], grid[i][1], grid[i][2], grid[i][3], grid[i][4], grid[i][5], grid[i][6], grid[i][7], grid[i][8]);
                 System.out.println(test); 
             }
-            if ((i+1) % 3 == 0){
-                System.out.println("+-----------------+");
+            if ((i+1) % 3 == 0) {
+                System.out.println("+-----------------+"); // Bottom line
             }
         }
         
@@ -178,7 +176,7 @@ class SudokuSolver {
         solve();
         if (solutionCounter > 1) {
             System.out.println(solutionCounter);
-        } else if (solutionCounter == 1){
+        } else if (solutionCounter == 1) {
             print();
         } else {
             System.out.println("no solution sir");
